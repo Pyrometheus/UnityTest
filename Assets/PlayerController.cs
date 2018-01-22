87261f0fb94773fb222fa2cd2771e247f3269f7d
Pyrometheus <orion.nye@gmail.com>
2018-01-21 18:40:16 -0700

    Game Started
    Added player and a floor, and a simple character controller

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed; //changes how fast the character moves
+    public float jump; //changes jump height
+    private Rigidbody2D rb2d;
+	// Use this for initialization
+	void Start() {
+        rb2d = GetComponent<Rigidbody2D>();	
+	}
+	
+	// Update is called once per frame
+	void Update() {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        rb2d.velocity = new Vector2(moveHorizontal * speed, rb2d.velocity.y);
+
+        if (Input.GetKeyDown("Space") == true)
+        {
+            rb2d.velocity = new Vector2(0, jump);
+        }
+	}
+}
